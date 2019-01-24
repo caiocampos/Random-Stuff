4e0359de414b90dca0a60b29accf1aeed56a2c8d
Caio Campos <caio.campos1204@gmail.com>
2019-01-24 17:38:41 -0200

    Exercícios exercism.io {Go}

---
+++ go/hamming.go
@@ -0,0 +1,16 @@
+package hamming
+
+import "errors"
+
+func Distance(a, b string) (int, error) {
+	if len(a) != len(b) {
+		return 0, errors.New("strands must be of equal length")
+	}
+	distance := 0
+	for i := range a {
+		if a[i] != b[i] {
+			distance++
+		}
+	}
+	return distance, nil
+}
