0c59f60a213588d50b72aa95d0888f91bd23b126
Caio Campos <caio.campos1204@gmail.com>
2019-01-26 16:00:20 -0200

    Exercícios exercism.io {Perl5}

--- perl/TwoFer.pm
+++ perl/TwoFer.pm
@@ -5,10 +5,16 @@ use Exporter 'import';
 our @EXPORT_OK = qw(two_fer);
 
 sub two_fer {
-  my ($name) = @_;
-  unless ($name) {
-    $name = "you"
-  }
+  my $name =  shift // "you";
+  # or
+  # my ($name) = @_;
+  # $name //= "you";
+  #
+  # or
+  # my ($name) = @_;
+  # unless ($name) {
+  #   $name = "you"
+  # }
   "One for $name, one for me."
 }
 
