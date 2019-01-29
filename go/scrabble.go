b157ccca3b4165f6396c49ca1366e33fdac14144
Caio Campos <caio.campos1204@gmail.com>
2019-01-29 13:45:06 -0200

    Exercícios exercism.io {Go}

--- go/scrabble.go
+++ go/scrabble.go
@@ -1,6 +1,6 @@
 package scrabble
 
-import "strings"
+import "unicode"
 
 // Score function computes the scrabble score for a word
 func Score(word string) int {
@@ -10,8 +10,8 @@ func Score(word string) int {
 // ScoreVA function computes the scrabble score for a word (Version A)
 func ScoreVA(word string) int {
 	res := 0
-	for _, letter := range strings.ToUpper(word) {
-		switch letter {
+	for _, letter := range word {
+		switch unicode.ToUpper(letter) {
 		case 'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T':
 			res++
 		case 'F', 'H', 'V', 'W', 'Y':
@@ -31,20 +31,21 @@ func ScoreVA(word string) int {
 	return res
 }
 
+var scores = map[rune]int {
+	'A': 1, 'E': 1, 'I': 1, 'O': 1, 'U': 1, 'L': 1, 'N': 1, 'R': 1, 'S': 1, 'T': 1,
+	'D': 2, 'G': 2,
+	'B': 3, 'C': 3, 'M': 3, 'P': 3,
+	'F': 4, 'H': 4, 'V': 4, 'W': 4, 'Y': 4,
+	'K': 5,
+	'J': 8, 'X': 8,
+	'Q': 10, 'Z': 10,
+}
+
 // ScoreVB function computes the scrabble score for a word (Version B)
 func ScoreVB(word string) int {
-	values := map[rune]int{
-		'A': 1, 'E': 1, 'I': 1, 'O': 1, 'U': 1, 'L': 1, 'N': 1, 'R': 1, 'S': 1, 'T': 1,
-		'D': 2, 'G': 2,
-		'B': 3, 'C': 3, 'M': 3, 'P': 3,
-		'F': 4, 'H': 4, 'V': 4, 'W': 4, 'Y': 4,
-		'K': 5,
-		'J': 8, 'X': 8,
-		'Q': 10, 'Z': 10,
-	}
 	res := 0
-	for _, letter := range strings.ToUpper(word) {
-		res += values[letter]
+	for _, letter := range word {
+		res += scores[unicode.ToUpper(letter)]
 	}
 	return res
 }
