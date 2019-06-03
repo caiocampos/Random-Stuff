e62d339d99420f10d7f51cf7fc5e2341d56e2911
Caio Campos <caio.campos1204@gmail.com>
2019-06-03 20:23:31 -0300

    Exercícios exercism.io {Go}

--- go/luhn.go
+++ go/luhn.go
@@ -1,12 +1,13 @@
 package luhn
 
 import (
+	"regexp"
 	"unicode"
 )
 
 // Valid function verifies if the input is valid per the Luhn formula
 func Valid(input string) bool {
-	return ValidVB(input)
+	return ValidVC(input)
 }
 
 // ValidVA function verifies if the input is valid per the Luhn formula (Version A)
@@ -43,10 +44,7 @@ func ValidVA(input string) bool {
 
 // ValidVB function verifies if the input is valid per the Luhn formula (Version B)
 func ValidVB(input string) bool {
-	count := 0
-	sumEven := 0
-	sumOdd := 0
-	val := 0
+	count, sumEven, sumOdd, val := 0, 0, 0, 0
 	even := true
 	for _, el := range input {
 		if unicode.IsNumber(el) {
@@ -78,4 +76,31 @@ func calculates(n int, double bool) int {
 		return n << 1 // val * 2
 	}
 	return n
+}
+
+var regexSpaces = regexp.MustCompile(`\s`)
+var regexNotDigit = regexp.MustCompile(`\s`)
+
+// ValidVC function verifies if the input is valid per the Luhn formula (Version C)
+func ValidVC(input string) bool {
+	vals := regexSpaces.ReplaceAllString(input, "")
+	if len(vals) < 2 || regexNotDigit.MatchString(vals) {
+		return false
+	}
+	double := len(vals)&1 == 0 // begin doubling if len(vals) is even
+	sum, val := 0, 0
+	for _, el := range vals {
+		val = int(el - '0') // int(el-'0') converts rune to int
+		if double {
+			if val > 4 { // if val > 4 then val * 2 > 9
+				sum += (val << 1) - 9 // val * 2 - 9
+			} else {
+				sum += val << 1 // val * 2
+			}
+		} else {
+			sum += val
+		}
+		double = !double
+	}
+	return sum%10 == 0
 }
