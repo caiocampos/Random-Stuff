c2d4b73c0d3bcde792db2b48dfb31c07456f9e02
Caio Campos <caio.campos1204@gmail.com>
2019-06-25 18:44:30 -0300

    Exercícios exercism.io {Python}

--- py/two_fer.py
+++ py/two_fer.py
@@ -1,9 +1,6 @@
-def two_fer(name=None):
-    return two_fer_vc(name)
-
 def two_fer_va(name=None):
     if name is None:
-        name = "you"
+        name = 'you'
     return f'One for {name}, one for me.'
 
 def two_fer_vb(name=None):
@@ -11,3 +8,8 @@ def two_fer_vb(name=None):
 
 def two_fer_vc(name=None):
     return f'One for {name or "you"}, one for me.'
+
+def two_fer_vd(name='you'):
+    return f'One for {name}, one for me.'
+
+two_fer = two_fer_vd
