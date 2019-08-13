6a246e827fc34298842675a937b1ce44e4b5f038
Caio Campos <caio.campos1204@gmail.com>
2019-08-12 21:51:38 -0300

    Exercícios exercism.io {Go}

--- go/tree.go
+++ go/tree.go
@@ -11,76 +11,37 @@ type Node struct {
 	Children []*Node
 }
 
-// Add method adds a Record to the Node hierarchy
-func (n *Node) Add(r Record) error {
-	if n.ID >= r.ID || n.ID > r.Parent {
-		return errors.New("Invalid Child ID")
-	}
-	if n.ID == r.Parent {
-		child := Node{ID: r.ID}
-		if n.Children == nil {
-			n.Children = []*Node{}
-		} else {
-			for _, child := range n.Children {
-				if child.ID == r.ID {
-					return errors.New("Duplicate Child ID")
-				}
-			}
-		}
-		n.Children = append(n.Children, &child)
-	} else { // n.ID < r.Parent
-		for _, child := range n.Children {
-			if err := child.Add(r); err == nil {
-				return nil
-			}
-		}
-		return errors.New("Parent not found")
-	}
-	return nil
-}
-
 // Record type defines a record to be restructured
 type Record struct {
 	ID     int
 	Parent int
 }
 
-// RecordList type defines a list of records that can be sorted
-type RecordList []Record
-
-// Len method returns the length of RecordList
-func (l RecordList) Len() int {
-	return len(l)
-}
-
-// Less method verifies if the element i is lesser than the element j of RecordList
-func (l RecordList) Less(i, j int) bool {
-	return l[i].ID < l[j].ID
-}
-
-// Swap method swaps the position of element i and j of RecordList
-func (l RecordList) Swap(i, j int) {
-	l[i], l[j] = l[j], l[i]
-}
-
 // Build function restructures the RecordList in a tree structure
-func Build(records RecordList) (*Node, error) {
-	if records.Len() == 0 {
+func Build(records []Record) (*Node, error) {
+	if len(records) == 0 {
 		return nil, nil
 	}
-	sort.Sort(records)
-	root, records := records[0], records[1:]
-	if root.ID != 0 || root.Parent > 0 {
+	sort.Slice(records, func(i, j int) bool {
+		return records[i].ID < records[j].ID
+	})
+	if records[0].ID != 0 || records[0].Parent > 0 {
 		return nil, errors.New("Invalid root")
 	}
-	node := Node{ID: 0}
+	nodes := make(map[int]*Node)
+	nodes[0] = &Node{ID: 0}
 	for i, record := range records {
-		if record.ID != i+1 {
+		if i == 0 {
+			continue
+		}
+		if record.ID != i {
 			return nil, errors.New("Non-continuous Tree")
 		}
-		if err := node.Add(record); err != nil {
-			return nil, err
+		if record.Parent >= i {
+			return nil, errors.New("Invalid Child ID")
 		}
+		nodes[i] = &Node{ID: i}
+		nodes[record.Parent].Children = append(nodes[record.Parent].Children, nodes[i])
 	}
-	return &node, nil
+	return nodes[0], nil
 }
