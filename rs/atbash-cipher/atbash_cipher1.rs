54799a45fe9baa97047fa71ea62be03d7d5a9d0f
Caio Campos <caio.campos1204@gmail.com>
2019-08-13 18:47:55 -0300

    Exercícios exercism.io {Rust}

--- rs/atbash-cipher/atbash_cipher1.rs
+++ rs/atbash-cipher/atbash_cipher1.rs
@@ -11,18 +11,19 @@ fn encode_char(c: char) -> Option<char> {
 
 /// Cipher a text with the Atbash cipher.
 pub fn encode(plain: &str) -> String {
-    plain
-        .chars()
-        .filter_map(encode_char)
-        .enumerate()
-        .flat_map(|(i, c)| {
-            if i > 0 && i % 5 == 0 {
-                vec![' ', c]
-            } else {
-                vec![c]
-            }
-        })
-        .collect()
+    let mut cipher = String::new();
+    let mut counter = 0;
+    for c in plain.chars() {
+        if counter == 5 {
+            cipher.push(' ');
+            counter = 0;
+        }
+        if let Some(e) = encode_char(c) {
+            cipher.push(e);
+            counter += 1;
+        }
+    }
+    cipher.trim_end().to_owned()
 }
 
 /// Decipher a text with the Atbash cipher.
